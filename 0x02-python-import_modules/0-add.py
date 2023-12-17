809a0cb4ad1155924e676461749254186c805cc2
amandamadlingozi <rjmadlingozi@gmail.com>
2023-12-17 11:34:56 +0200

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+# 0_add.py
+<<<<<<< HEAD
+def add(a,b):
+=======
+>>>>>>> ae00f1b55f7e4637264af385d5f7a1b76d858214
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    

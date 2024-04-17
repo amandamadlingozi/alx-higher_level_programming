48f1925711f0f83579ae5f8f1d84e088be509f09
amandamadlingozi <rjmadlingozi@gmail.com>
2024-04-17 18:35:56 +0200

    changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,114 +1,67 @@
 #!/usr/bin/python3
-"""First Rectangle"""
-from .base import Base
+"""Defines the Rectangle class."""
 
+from models.base import Base
 
 class Rectangle(Base):
-    """Defines a Rectangle"""
-
-    @property
-    def width(self):
-        return (self.__width)
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return (self.__height)
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return (self.__x)
-
-    @x.setter
-    def x(self, value):
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return (self.__y)
-
-    @y.setter
-    def y(self, value):
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+    """Rectangle class inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes object"""
+        """Initialize Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    def area(self):
-        """Computes area of rectangle"""
-        return self.__width * self.__height
+        @property
+        def width(self):
+            """Getter for width attribute."""
+            return self.__width
 
-    def display(self):
-        for y in range(self.__y):
-            print()
-        for i in range(self.__height):
-            for x in range(self.__x):
-                print(" ", end="")
-            for j in range(self.__width):
-                print("#", end="")
-            print()
+        @width.setter
+        def width(self, value):
+            """Setter for width attribute."""
+            self.__width = value
 
-    def __str__(self):
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
-                self.id, self.__x, self.__y, self.__width, self.__height)
+            @property
+            def height(self):
+                """Getter for height attribute."""
+                return self.__height
 
-    def update(self, *args, **kwargs):
-        if args is not None:
-            for i in range(len(args)):
-                if i == 0:
-                    self.id = args[i]
-                elif i == 1:
-                    self.__width = args[i]
-                elif i == 2:
-                    self.__height = args[i]
-                elif i == 3:
-                    self.__x = args[i]
-                elif i == 4:
-                    self.__y = args[i]
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                elif key == "width":
-                    self.__width = value
-                elif key == "height":
-                    self.__height = value
-                elif key == "x":
+            @height.setter
+            def height(self, value):
+                """Setter for height attribute."""
+                self.__height = value
+
+                @property
+                def x(self):
+                    """Getter for x attribute."""
+                    return self.__x
+
+                @x.setter
+                def x(self, value):
+                    """Setter for x attribute."""
                     self.__x = value
-                elif key == "y":
-                    self.__y = value
 
-    def to_dictionary(self):
-        return {"id": self.id,
-                "width": self.__width,
-                "height": self.__height,
-                "x": self.__x,
-                "y": self.__y}
+                    @property
+                    def y(self):
+                        """Getter for y attribute."""
+                        return self.__y
+
+                    @y.setter
+                    def y(self, value):
+                        """Setter for y attribute."""
+                        self.__y = value
+
+                        # Test cases
+                        if __name__ == "__main__":
+                            r1 = Rectangle(10, 2)
+                            print(r1.id)
+
+                            r2 = Rectangle(2, 10)
+                            print(r2.id)
+
+                            r3 = Rectangle(10, 2, 0, 0, 12)
+                            print(r3.id)
+
